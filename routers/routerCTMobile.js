b8d6c1793b3cf5c587cb07d9bb14480102319379
Gic0 <g.zdanovich@onpoint.ru>
2020-09-08 17:17:52 +0300

    add slidesOrder to CT mobile

--- routers/routerCTMobile.js
+++ routers/routerCTMobile.js
@@ -1,26 +1,25 @@
 const Router = require('@onpoint-dev/mpa-router/routers/router');
 
 class RouterCTMobile extends Router {
-
-  initSwipeHandlers() {
-    this.__swipeLeftEvent = new Event('swipeleft', { bubbles: true })
-    this.__swipeRightEvent = new Event('swiperight', { bubbles: true })
+  constructor(settings) {
+    super(settings);
+    this.__slidesOrder = window.slidesOrder;
   }
 
   __routerAdapter(slide, scene, presentation) {
-//     const slideOrder = this.__slidesToOrderRel[slide]
-//     if (slideOrder === undefined)
-//       throw new Error(`
-// Для слайда "${slide}" нет порядкового номера, по которому приложение должно будет перейти.
-// Добавь этот номер в "src/pres_config/slidesToOrderRel.js".
-//     `)
-    if (this.__isChrome) {
-      // console.log(slideOrder)
-      document.location.pathname = `${presentation}/${presentation}_${slide}.html`
-    } else {
-      console.log(slide, slideOrder)
-      CTAPPgoToSlide(slideOrder)
-    }
+    const slideOrder = this.__slidesToOrderRel[slide];
+    if (slideOrder === undefined)
+      if (this.__isChrome) {
+        //       throw new Error(`
+        // Для слайда "${slide}" нет порядкового номера, по которому приложение должно будет перейти.
+        // Добавь этот номер в "src/pres_config/slidesToOrderRel.js".
+        //      `);
+        // console.log(slideOrder)
+        document.location.pathname = `${presentation}/${presentation}_${slide}.html`;
+      } else {
+        console.log(slide, slideOrder);
+        CTAPPgoToSlide(slideOrder);
+      }
   }
 }
 
