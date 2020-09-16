11ce628f9c37952ca6f80cf403cba4bf8d10c668
Lev <agams92@gmail.com>
2020-09-16 09:55:35 +0300

    добавил папку build в роутинг

--- routers/routerResco.js
+++ routers/routerResco.js
@@ -2,10 +2,13 @@ const Router = require('./router');
 
 class RouterResco extends Router {
   __routerAdapter(slide, scen, presentation) {
+    const { scenario } = this.__currPresConfig;
+    const [initSlide] = scenario[this.__defaultScene];
     super.__setCurrScen(scen);
+    const newSlide = slide === initSlide ? 'index' : slide;
     const isDesktop = navigator.platform === 'Win32' || navigator.platform === 'MacIntel';
-    if (isDesktop) document.location = `/${presentation}/${slide}.html#${scen}`;
-    else window.location = `${slide}.html`;
+    if (isDesktop) window.location = `/${presentation}/build/${newSlide}.html#${scen}`;
+    else window.location = `${newSlide}.html`;
   }
 }
 
