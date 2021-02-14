62937963d4620dcd1b7553c41254b83c40745665
Aleksandr <a.borisov@onpoint.ru>
2021-02-14 20:59:44 +0300

    fix pitcher router

--- routers/routerPitcher.js
+++ routers/routerPitcher.js
@@ -3,15 +3,14 @@ const Router = require('@onpoint-dev/mpa-router/routers/router');
 class RouterPitcher extends Router {
   constructor(settings) {
     super(settings);
-    this.hrefOrigin = document.location.href.split("/" + CURRENT_PRESENTATION + "/")[0];
+    this.hrefOrigin = document.location.href.split('/' + CURRENT_PRESENTATION + '/')[0];
   }
 
   __routerAdapter(slide, scen, presentation) {
     const newSlide = `${presentation}_${slide}`;
     super.__setCurrScen(scen);
 
-    if (this.__isChrome) document.location = `/${presentation}/${newSlide}/${newSlide}.html#${scen}`;
-    else document.location = this.hrefOrigin + "/" + presentation + "/" + newSlide + "/" + newSlide + `.html#${scen}`;  
+    document.location = `${this.hrefOrigin}/${presentation}/${newSlide}/${newSlide}.html#${scen}`;
   }
 }
 
