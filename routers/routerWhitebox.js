925d149fca11567ed37624581b1d95d839f802e5
Vladislav <v.geyer@onpoint.ru>
2021-03-02 14:05:38 +0300

    убрал отключение навигации

--- routers/routerWhitebox.js
+++ routers/routerWhitebox.js
@@ -4,12 +4,6 @@ class RouterWhitebox extends Router {
   constructor(settings) {
     super(settings);
 
-    try {
-      CommunicateEmbedded.suppressNavigation();
-    } catch (e) {
-      console.log(e);
-    }
-
     this.__slidesNames = window.slidesNames;
   }
 
@@ -21,7 +15,10 @@ class RouterWhitebox extends Router {
       throw new Error(`Данный слайд "${slide}" отсутствует!`);
     }
 
-    if (this.__isChrome && navigator.platform === 'Win32') {
+    if (
+      (this.__isChrome && window.navigator.platform === 'Win32') ||
+      (this.__isChrome && window.navigator.platform.includes('Mac'))
+    ) {
       document.location = `/${presentation}/${slideName}/${slideName}.html#`;
     } else {
       CommunicateEmbedded.navigate(this.__slidesNames[slide]);
